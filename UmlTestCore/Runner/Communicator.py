74a4d2975d7bac446c7ef3337212f9b3dbebf668
kai_Ker <kai_Ker@buaa.edu.cn>
2024-05-25 22:16:30 +0800

    [Feat] Subprocess runner and communicator

---
+++ UmlTestCore/Runner/Communicator.py
@@ -0,0 +1,55 @@
+import subprocess
+from typing import List
+from enum import Enum
+
+
+class Action(Enum):
+    SendText = 1
+    Continue = 2
+    Terminate = 3
+
+
+class Reaction:
+    def __init__(self, action: Action, msg: str = "") -> None:
+        self.action: Action = action
+        self.msg: str = msg
+
+
+def runner_core(args: List[str], callback, init_input: List[str], store_pattern: str):
+    process = subprocess.Popen(
+        args=args,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True
+    )
+    stdin = process.stdin
+    stdout = process.stdout
+
+    if stdin is None or stdout is None:
+        return "Fail To Start!"
+
+    fin = open(store_pattern.replace("%%", "input"), 'w', encoding="utf-8")
+    fout = open(store_pattern.replace("%%", "output"), 'w', encoding="utf-8")
+
+    for command in init_input:
+        command += '\n'
+        fin.write(command)
+        stdin.write(command)
+    stdin.flush()
+
+    while True:
+        output = stdout.readline().removesuffix('\n')
+        reaction: Reaction = callback(output)
+        if reaction.action == Action.Continue:
+            continue
+        if reaction.action == Action.Terminate:
+            break
+        if reaction.action == Action.SendText:
+            stdin.write(reaction.msg + '\n')
+            stdin.flush()
+
+    fin.close()
+    fout.close()
+    stdin.close()
+    process.kill()
