0e57b2519281ca3838432132be0a35d9206e059f
kai_Ker <kai_Ker@buaa.edu.cn>
2024-05-26 14:31:38 +0800

    [Feat] Main interaction

--- UmlMain.py
+++ UmlMain.py
@@ -3,10 +3,13 @@ from UmlTestCore.Runner.Communicator import runner_core
 from UmlTestCore.Core import Core
 
 import sys
+import shutil
 from pathlib import Path
+from time import time
 
+def main():
+    Path("TestCases/").mkdir(exist_ok=True)
 
-if __name__ == "__main__":
     # Handle the config
     config = read_config()
     if config is None:
@@ -22,6 +25,41 @@ if __name__ == "__main__":
         print(f"Cannit find the Jar: {jar_path}")
         sys.exit(2)
 
-    core = Core(config)
-    ret = runner_core([java_path, '-jar', jar_path], core.command_callback, core.gen_init_command(), '%%.txt')
-    print(ret)
+    test_num_s = input("Test Count, 0 for endless tests.\n> ")
+    if not test_num_s.isnumeric():
+        print("Invalid.")
+        sys.exit(3)
+    test_num = int(test_num_s)
+    if test_num < 0:
+        print("Invalid.")
+        sys.exit(3)
+    if test_num == 0:
+        print("Launch end-less tests")
+
+    i = 1
+    wa = 0
+    while i <= test_num or test_num == 0:
+        print(f"#({i}/{str(test_num) if test_num > 0 else 'endless'}) WA: {wa} cases\n...", end='')
+        sys.stdin.flush()
+        core = Core(config)
+        ret = runner_core([java_path, '-jar', jar_path], core.command_callback, core.gen_init_command(), 'TestCases/%%.txt')
+        print(ret)
+        if ret != "Ok":
+            dump_core = core.library.core_dump()
+            time_s = time.strftime('%m-%d-%H-%M-%S', time.localtime())
+            wa += 1
+            shutil.copy('TestCases/input.txt', f'TestCases/{time_s}.in.txt')
+            shutil.copy('TestCases/output.txt', f'TestCases/{time_s}.out.txt')
+            Path(f'TestCases/{time_s}.dump.txt').write_text(dump_core)
+            Path(f'TestCases/{time_s}.check.txt').write_text(ret)
+        i += 1
+
+    if wa == 0:
+        print("All Accepted!")
+
+
+try:
+    main()
+except KeyboardInterrupt:
+    print("\n\nYou are going to terminate me now?")
+    print("Farewell!")
